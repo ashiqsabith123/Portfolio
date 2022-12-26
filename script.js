ea55fb4f16316de6f2fc5c80bf7eb6ae746d2df8
Ashiq Sabith <ashiqsabith328@gmail.com>
2022-12-26 17:15:47 +0530

    Completed

--- script.js
+++ script.js
@@ -1,28 +1,4 @@
-// $(document).ready(function(){
-//     $("#submit-form").validate({
-//         rules:{
-//             name:{
-//                 required:true,
-//                 minlength:4
-//             },
 
-//             email:{
-//                 required: true,
-//                 email:true
-//             },
-
-//             color:{
-//                 required: true,
-//                 minlength:10
-//             },
-            
-//             message:{
-//                 required:true,
-//                 minlength:10
-//             }
-//         }
-//     })
-// })
 
 
 
@@ -67,20 +43,3 @@ $("#submit-form").validate({
 });
 
 
-// $("#submit-form").submit((e)=>{
-//     e.preventDefault()
-//     $.ajax({
-//         url:"https://script.google.com/macros/s/AKfycbw157c2zP6HQebbTRtPvrFqNvUJmxcC0ldaok097DdR_R1xM8_cmFPjATmOHmHb0ZkX/exec",
-//         data:$("#submit-form").serialize(),
-//         method:"post",
-//         success:function (response){
-//             alert("Form submitted successfully")
-//             window.location.reload()
-//             //window.location.href="https://google.com"
-//         },
-//         error:function (err){
-//             alert("Something Error")
-
-//         }
-//     })
-// })
